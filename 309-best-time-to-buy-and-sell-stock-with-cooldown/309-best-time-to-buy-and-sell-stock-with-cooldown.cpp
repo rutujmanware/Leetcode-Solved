b26531708d44b5a50d691eded5f14f3a51875c05
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2022-08-27 15:45:28 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (35.86%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -16,7 +16,23 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return calprofit(prices,0,1,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        // return calprofit(prices,0,1,dp);
+        for(int ind = n-1; ind>=0 ; ind--){
+            for(int buy = 0;buy <=1 ; buy++){
+                int profit = 0;
+            if(buy){
+                profit = max(-prices[ind] + dp[ind+1][0],dp[ind+1][1]);
+            }
+            else{
+                profit = max(prices[ind] + dp[ind+2][1],dp[ind+1][0]);
+            }
+            dp[ind][buy] = profit;
+            
+            }
+        }
+    
+        
+        return dp[0][1];
     }
 };
