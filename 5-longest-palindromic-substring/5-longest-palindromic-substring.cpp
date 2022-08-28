82a8fa0486979c797d873146474cda0b4933cfc2
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2022-08-28 10:12:49 +0530

    Time: 41 ms (70.24%), Space: 19.2 MB (48.07%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res="";
+        int maxi = 0;
+      
+        for(int i=0;i<s.size();i++){
+            int l=i,r=l;
+            
+                while(l>=0 && r<s.size() && s[l]==s[r]){
+                    if((r-l+1)>maxi){
+                        maxi = r-l+1;
+                        res = s.substr(l,r-l+1);
+                    }
+                    l--;
+                    r++;
+                }
+            
+            l=i;
+            r=l+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if((r-l+1)>maxi){
+                    maxi = r-l+1;
+                    res = s.substr(l,r-l+1);
+                }
+                l--;
+                r++;
+            }
+        
+        
+    }
+          return res;  
+    }
+};
