9a0b82532382e7f81bb27b936639ac9430c7968f
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2022-09-15 10:35:44 +0530

    Time: 416 ms (86.73%), Space: 192.2 MB (33.47%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,47 +11,54 @@
  */
 class Solution {
 public:
-     int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
-        if (!root) return 0;
-        count ^= 1 << (root->val - 1);
-        int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
-        if (root->left == root->right && (count & (count - 1)) == 0) res++;
-        return res;
-    }
-//     void getpath(TreeNode* root,map<int,int> mp,int &ans){
-//         if(!root){
-//             return ;
-//         }
-//         mp[root->val]++;
-//         if(!root->left && !root->right){
-//             int even=0,odd=0;
-//             for(auto &val:mp){
-//                 if(val.second%2==0){
-//                     even++;
-//                 }
-//                 else{
-//                     odd++;
-//                 }
-//             }
-//             if(odd==1 || odd == 0){
-//                 ans++;
-//             }
+    //  int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
+    //     if (!root) return 0;
+    //     count ^= 1 << (root->val - 1);
+    //     int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
+    //     if (root->left == root->right && (count & (count - 1)) == 0) res++;
+    //     return res;
+    // }
+    void getpath(TreeNode* root,map<int,int> &mp,int &ans){
+        if(!root){
+            return ;
+        }
+        mp[root->val]++;
+        if(!root->left && !root->right){
+            int odd=0;
+            bool flag = 1;
+            for(auto &val:mp){
+            if(val.second%2!=0 && flag){
+                    odd++;
+                    
+                    if(odd>1){
+                        flag = 0;
+                        break;
+                    }
+                }
+               
+            }
+            mp[root->val]--;
+            if(flag){
+                ans++;
+            }
+            return;
             
-//         }
+        }
     
-//             getpath(root->left,mp,ans);
-//             getpath(root->right,mp,ans);
+            getpath(root->left,mp,ans);
+            getpath(root->right,mp,ans);
+        mp[root->val]--;
         
-//     }
-//     int pseudoPalindromicPaths (TreeNode* root) {
-//         vector<vector<int>> list;
-//         int ans = 0;
-//         map<int,int> mp;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<vector<int>> list;
+        int ans = 0;
+        map<int,int> mp;
         
-//         getpath(root,mp,ans);
-//         return ans;
-//         int ans = 0;
-//         // cout<<list.size()<<endl;
+        getpath(root,mp,ans);
+        return ans;
+        // int ans = 0;
+        // cout<<list.size()<<endl;
 //         for(int i=0;i<list.size();i++){
 //             map<int,int> m;
 //             bool flag = 1;
@@ -91,5 +98,5 @@ public:
 //             }
 //         }
 //         return ans;
-    // }
+    }
 };
