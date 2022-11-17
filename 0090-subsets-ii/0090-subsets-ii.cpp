53bbe22db6d2862d6e07b798b70dc7c1b5b60f3a
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2022-11-17 21:19:05 +0530

    Time: 8 ms (41.49%), Space: 7.4 MB (96.44%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> go;
+        // ans.push_back(go);
+        rec(0,nums,go,ans);
+        return ans;
+        
+    }
+    void rec(int ind,vector<int> &nums,vector<int> &go,vector<vector<int>> &ans){
+        // if(ind==nums.size()) 
+        ans.push_back(go);
+        for(int i=ind;i<nums.size();i++){
+            if(i==ind || nums[i]!=nums[i-1]){
+                go.push_back(nums[i]);
+                rec(i+1,nums,go,ans);
+                go.pop_back();
+            }
+            
+        }
+    }
+       
+};
