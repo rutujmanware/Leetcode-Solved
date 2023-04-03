c11a92d7a8736d2e2b596cf4c6e5c6239a4b3f26
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-04-03 19:27:30 +0530

    Time: 119 ms (5.02%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -3,17 +3,17 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
        sort(people.rbegin(),people.rend());
         int count=0;
-        int pos=0;
-        for(int i=0;i<people.size();i++){
-            if(people[i]==limit){
-                count++;
-            }
-            else{
-                pos=i;
-                break;
-            }
-        }
-        int i=pos;
+        // int pos=0;
+        // for(int i=0;i<people.size();i++){
+        //     if(people[i]==limit){
+        //         count++;
+        //     }
+        //     else{
+        //         pos=i;
+        //         break;
+        //     }
+        // }
+        int i=0;
         int j = people.size()-1;
         while(i<=j){
             if(people[i]+people[j]<=limit && i!=j){
