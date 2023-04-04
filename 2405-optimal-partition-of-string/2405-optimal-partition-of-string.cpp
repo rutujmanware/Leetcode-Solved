cf4a4a6e850beb0d85db143fd7e1e93d000fa013
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-04-04 11:37:51 +0530

    Time: 20 ms (93.72%), Space: 10.3 MB (90.34%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int partitionString(string s) {
-        map<char,int> m;
-        int count=1;
-        for(auto &S:s){
-            if(m.count(S)){
+//         map<char,int> m;
+//         int count=1;
+//         for(auto &S:s){
+//             if(m.count(S)){
+//                 count++;
+//                 m.clear();
+//             }
+//             m[S] = 1;
+            
+//         }
+//         return count;
+//     }
+        int flag = 0,count = 1;
+        for(int i=0;i<s.length();i++){
+            int k = s[i] - 'a';
+            if(flag&(1<<k)){
                 count++;
-                m.clear();
+                flag=0;    
             }
-            m[S] = 1;
+            flag = flag|(1<<k);
             
         }
         return count;
