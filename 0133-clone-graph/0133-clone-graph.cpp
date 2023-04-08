03d2820b03a7f2074ea46fa95b21e42fe6b9367b
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-04-08 18:39:28 +0530

    Time: 3 ms (95.03%), Space: 8.6 MB (87.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,52 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
-    public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   
-                {
-                    neighbour.push_back(mp[it]);    
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
-            }
-            clone->neighbors=neighbour;
-            return clone;
-    }
+public:
+    unordered_map<Node* , Node*> mp; // declaring map, to check whwther we have a copy of node or not and also to store copy
+    
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)   
+        if(node == NULL) // if node is null, then simply return null
         {
-            Node* clone= new Node(node->val);
-            return clone; 
+            return NULL;
         }
-        return dfs(node,mp);
+        
+        Node* first = new Node(node -> val, {}); // make a copy of first node
+        mp[node] = first;
+        
+        queue<Node*> q;
+        q.push(node); 
+        
+        while(q.empty() == false) 
+        {
+            Node* curr = q.front(); 
+            q.pop();
+            
+            for(auto adj: curr -> neighbors) 
+            {
+                if(mp.find(adj) == mp.end()) 
+                {
+                    mp[adj] = new Node(adj -> val, {}); 
+                    q.push(adj); 
+                    
+                }
+                
+                mp[curr] -> neighbors.push_back(mp[adj]); 
+            }
+        }
+        
+        return mp[node];
     }
 };
