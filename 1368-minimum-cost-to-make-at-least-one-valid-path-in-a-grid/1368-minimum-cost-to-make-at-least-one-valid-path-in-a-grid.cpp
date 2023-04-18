b1f98149389487f2360c8286572f6660c3e48397
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-04-18 21:59:32 +0530

    Time: 59 ms (75.97%), Space: 12.9 MB (96.49%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,29 +5,37 @@ public:
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         pq.push({0,0});
         int x,y;
-        vector<int> vis(n*m,0);
+        vector<int> cost(n*m,INT_MAX);
         while(!pq.empty()){
             auto [wt,node] = pq.top();
             // auto [x,y] = node;
             pq.pop();
             x = node/m;
             y = node%m;
-            if(vis[node]){
-                continue;
-            }
+            // if(vis[node]){
+            //     continue;
+            // }
             if(x==n-1 && y==m-1){
                 return wt;
             }
             
-            vis[node] = 1;
-            if(y+1<m && !vis[node+1])
+            // vis[node] = 1;
+            if(y+1<m && wt+!(grid[x][y]==1) < cost[node+1]){
                 pq.push({wt+!(grid[x][y]==1),node+1});
-            if(y-1>=0 && !vis[node-1])
+                cost[node+1] = wt+!(grid[x][y]==1);
+            }
+            if(y-1>=0 && wt+!(grid[x][y]==2)<cost[node-1]){
                 pq.push({wt+!(grid[x][y]==2),node-1});
-            if(x+1<n && !vis[node+m])
+                cost[node-1] = wt+!(grid[x][y]==2);
+            }
+            if(x+1<n && wt+!(grid[x][y]==3)<cost[node+m]){
                 pq.push({wt+!(grid[x][y]==3),node+m});
-            if(x-1>=0 && !vis[node-m])
+                cost[node+m] = wt+!(grid[x][y]==3);
+            }
+            if(x-1>=0 && wt+!(grid[x][y]==4)<cost[node-m]){
                 pq.push({wt+!(grid[x][y]==4),node-m});
+                cost[node-m] = wt+!(grid[x][y]==4);
+            }
         }
         return 0;
     }
