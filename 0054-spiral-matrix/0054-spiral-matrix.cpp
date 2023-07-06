307e582347aef3216b3f9310585962b896d4281b
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-06 18:02:03 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (99.94%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,63 +1,74 @@
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int n = matrix.size(), m = matrix[0].size();
-        int i=0,j=0,k=0,l=0;
-        while(true){
-            if(j<m){
-               for( j;j<m;j++){
+class Solution
+    {
+    public:
+        vector<int> spiralOrder(vector<vector<int>> &matrix)
+        {
+            vector<int> ans;
+            int n = matrix.size(), m = matrix[0].size();
+            int i = 0, j = 0, k = 0, l = 0;
+            while (true)
+            {
+                if (j < m)
+                {
+                    for (j; j < m; j++)
+                    {
                         ans.push_back(matrix[i][j]);
+                    }
                 }
-            }
-            else{
-                break;
-            }
-            j--;
-            i++;
-            if(i<n){
-                for(i;i<n;i++){
-                    ans.push_back(matrix[i][j]);
-                }
-            }
-            else {
-                break;
-            }
-            i--;
-            j--;
-            if(j>=k){
-                
-            
-                for(j;j>=k;j--){
-                    ans.push_back(matrix[i][j]);
-                }
-            }
-            else {
-                break;
-            }
-            i--;
-            j++;
-            if(i>l){
-                
-            
-                for(i;i>l;i--){
-                    ans.push_back(matrix[i][j]);
-                }
-            }
-                else{
+                else
+                {
                     break;
                 }
-            i++;
-            j++;
-            
-            n--;
-            m--;
-            l++;
-            k++;
-            // if(n==l && m==k){
-            //     break;
-            // }  
+                j--;
+                i++;
+                if (i < n)
+                {
+                    for (i; i < n; i++)
+                    {
+                        ans.push_back(matrix[i][j]);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                i--;
+                j--;
+                if (j >= k)
+                {
+
+                    for (j; j >= k; j--)
+                    {
+                        ans.push_back(matrix[i][j]);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                i--;
+                j++;
+                if (i > l)
+                {
+
+                    for (i; i > l; i--)
+                    {
+                        ans.push_back(matrix[i][j]);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+                j++;
+
+                n--;
+                m--;
+                l++;
+                k++;
+                
+            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
