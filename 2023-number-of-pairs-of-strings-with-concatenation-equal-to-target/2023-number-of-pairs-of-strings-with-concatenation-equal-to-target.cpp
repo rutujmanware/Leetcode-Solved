1d69450a8a2bf959b784b9196b6afd48ea5baf86
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-06 18:18:59 +0530

    Time: 7 ms (96.39%), Space: 10.2 MB (95.41%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     
-    bool isEqual(string s,string target){
-        if(s==target){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+    // bool isEqual(string s,string target){
+    //     if(s==target){
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
     int numOfPairs(vector<string>& nums, string target) {
-        int ans=0;
+        // int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=0;j<nums.size();j++){
+        //         if(i==j) continue;
+        //         string s = nums[i] + nums[j];
+        //         if(isEqual(s,target)){
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        unordered_map<string,int> m;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(i==j) continue;
-                string s = nums[i] + nums[j];
-                if(isEqual(s,target)){
-                    ans++;
-                }
+            if(nums[i].size()>=target.size()){
+                continue;
+            }
+            m[nums[i]]++;
+        }
+        int ans = 0;
+        for(auto &val:m){
+            if(val.first==target.substr(0,val.first.size())){
+                string s = target.substr(val.first.size());
+                ans += (val.first==s)?(val.second-1)*val.second:val.second*m[s];
             }
         }
         return ans;
+        
     }
 };
