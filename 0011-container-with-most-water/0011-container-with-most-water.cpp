5d47604d5744090a1c6d5b184f4f3b4267065379
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-13 12:09:52 +0530

    Time: 95 ms (88.31%), Space: 59.1 MB (5.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,7 +7,17 @@ public:
         int area = 0;
         while(left<right){
             int ta = min(height[left],height[right])*(right-left);
-            (height[left]<height[right])? left++:right--;
+            int hl = height[left],hr=height[right];
+            if(hl<hr){
+                while( left<n && hl>=height[left]){
+                    left++;
+                }
+            }
+            else{
+                while(right>=0 && hr>=height[right]){
+                    right--;
+                }
+            }
             area = max(ta,area);
         }
         return area;
