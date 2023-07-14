182f323f1bafe6a0b755c0b6ff8764e8f387b758
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-14 12:02:54 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (70.58%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -5,7 +5,7 @@ public:
         int m = board[0].size();
         vector<int> dirx = {0,0,1,-1,1,-1,1,-1};
         vector<int> diry = {1,-1,0,0,1,-1,-1,1};
-        vector<vector<int>> h(n,vector<int>(m));
+        // vector<vector<int>> h(n,vector<int>(m));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int live = 0;
@@ -13,25 +13,30 @@ public:
                     int newi = i + dirx[k];
                     int newj = j + diry[k];
                     if(newi<0 || newi>=n || newj<0 || newj>=m) continue;
-                    if(board[newi][newj] == 1){
+                    if(abs(board[newi][newj]) == 1){
                         live++;
                     }
                 }
                 // cout<<live<<endl;
                 if(board[i][j] == 0 && live==3){
-                    h[i][j] = 1;
+                    board[i][j] = 2;
                 }
-                else if(board[i][j] == 1 && live<2){
-                    h[i][j] = 0;
+                else if(board[i][j] == 1 && (live<2 || live>3)){
+                    board[i][j] = -1;
                 }
-                else if(board[i][j] == 1 && live>3){
-                h[i][j] = 0;
+                
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == 2){
+                    board[i][j] = 1;
                 }
-                else{
-                    h[i][j] = board[i][j];
+                else if(board[i][j] == -1){
+                    board[i][j] = 0;
                 }
             }
         }
-        board = h;
+        
     }
 };
