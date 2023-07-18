95983eff074da25340a24865713f15aef2473cfd
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-18 19:14:17 +0530

    Time: 12 ms (93.76%), Space: 16.3 MB (99.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int help(int ind, vector<int> &nums, vector<int> &dp){
-       if(ind >= nums.size()-1){
-           return 0;
-       }
-        int ans = INT_MAX-1;
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
+//     int help(int ind, vector<int> &nums, vector<int> &dp){
+//        if(ind >= nums.size()-1){
+//            return 0;
+//        }
+//         int ans = INT_MAX-1;
+//         if(dp[ind] != -1){
+//             return dp[ind];
+//         }
         
-        for(int i=1;i<=nums[ind];i++){
+//         for(int i=1;i<=nums[ind];i++){
             
-            int jump = help(ind+i,nums,dp) + 1;
-            ans = min(ans,jump);
+//             int jump = help(ind+i,nums,dp) + 1;
+//             ans = min(ans,jump);
+            
+//         }
+//         return dp[ind] = ans;
+//     }
+    int jump(vector<int>& nums) {
+        // vector<int> dp(nums.size(),-1);
+        // int ans = help(0,nums,dp);
+        // return ans;  
+        int farthest = 0,current = 0,jumps=0;
+        
+        for(int i=0;i<nums.size()-1;i++){
+            farthest = max(farthest,nums[i]+i);
+            if(current == i){
+                current = farthest;
+                jumps++;
+            }
             
         }
-        return dp[ind] = ans;
-    }
-    int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        int ans = help(0,nums,dp);
-        return ans;  
+        return jumps;
+        
     }
 };
