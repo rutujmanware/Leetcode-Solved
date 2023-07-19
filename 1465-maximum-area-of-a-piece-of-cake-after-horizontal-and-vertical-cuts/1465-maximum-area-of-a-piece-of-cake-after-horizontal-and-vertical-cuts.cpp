2155fd44195863dc3fc80bbd496fb37ab1e00fd4
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-19 19:47:05 +0530

    Time: 57 ms (99.80%), Space: 32.9 MB (12.09%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int find_max_diff(vector<int> v,int l){
+       
         sort(v.begin(),v.end());
         int maxdiff = v[0];
         for(int i=1;i<v.size();i++){
@@ -10,6 +11,9 @@ public:
         return maxdiff;
     }
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+         ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         long maxh = find_max_diff(horizontalCuts,h);
         long maxw = find_max_diff(verticalCuts,w);
         return (maxh*maxw)%1000000007;
