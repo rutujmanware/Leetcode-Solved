a40755486335a6850e7db9a1302e7dba77c9a3ae
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-20 18:26:15 +0530

    Time: 67 ms (70.05%), Space: 33.6 MB (42.68%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        // int n = nums.size();
+        // unordered_map<int,int> m;
+        // for(int i=0;i<n;i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto &v:m){
+        //     if(v.second==2){
+        //         ans.push_back(v.first);
+        //     }
+        // }
+        //     return ans;
         int n = nums.size();
-        unordered_map<int,int> m;
-        for(int i=0;i<n;i++){
-            m[nums[i]]++;
-        }
         vector<int> ans;
-        for(auto &v:m){
-            if(v.second==2){
-                ans.push_back(v.first);
+        for(int i=0;i<n;i++){
+            if(nums[abs(nums[i]) -1 ] <0){
+                ans.push_back(abs(nums[i]));
+            }
+            else{
+                nums[abs(nums[i])-1] = -nums[abs(nums[i])-1];
             }
         }
-            return ans;
+        return ans;
     }
 };
