160f052a445fd9d12601bafc904058ff7b168e58
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-07-23 14:05:50 +0530

    Time: 184 ms (85.26%), Space: 77.1 MB (79.38%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        if(n == 0) return 0; // Base Condition
-        int maxArea = 0;
-        vector<int> left(n); //fill left boundary
-        vector<int> right(n); // fill right boundary
-        
-        left[0] = -1;
-        right[n - 1] = n;
-        
-        for(int i = 1; i < n; i++){
-            int prev = i - 1; // previous for comparing the heights
-            while(prev >= 0 && heights[prev] >= heights[i]){
-                prev = left[prev]; // we have done this to minimise the jumps we make to the left
+        int n = heights.size() , max_area = INT_MIN , i = 0 ; 
+        stack<int> s ;
+        while(i < n) {
+            if(s.empty() || heights[s.top()] <= heights[i]) {
+                s.push(i) ; 
+                i++ ; 
+            } else {
+                int prev_top = s.top() ; 
+                s.pop() ; 
+                max_area = max(max_area , heights[prev_top] * (s.empty() ? i : i - s.top() - 1)) ;
             }
-            left[i] = prev;
         }
-        // Similarly we do for right
-        for(int i = n - 2; i >= 0; i--){
-            int prev = i + 1; 
-            while(prev < n && heights[prev] >= heights[i]){
-                prev = right[prev]; 
-            }
-            right[i] = prev;
-        }
-        // once we have these two arrays fill we need width & area
-        for(int i = 0; i < n; i++){
-            int width = right[i] - left[i] - 1;
-            maxArea = max(maxArea, heights[i] * width);
-        }
-        return maxArea;
+        while(!s.empty()) {
+            int prev_top = s.top() ; 
+            s.pop() ; 
+            max_area = max(max_area , heights[prev_top] * (s.empty() ? i : i - s.top() - 1)) ;
+        }  
+        return max_area ;
     }
 };
