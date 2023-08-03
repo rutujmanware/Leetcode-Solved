51a204b929b38c39125a191b0d6a027638aff47e
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-08-04 05:07:05 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (55.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,15 +1,26 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n = needle.size();
+//         if(haystack.size()<needle.size()) return -1;
+//         for(int i=0;i<=haystack.size()-n;i++){
+//             string f = haystack.substr(i,n);
+//             if(f==needle){
+//                 return i;
+//             }
+            
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n = needle.size();
-        if(haystack.size()<needle.size()) return -1;
-        for(int i=0;i<=haystack.size()-n;i++){
-            string f = haystack.substr(i,n);
-            if(f==needle){
-                return i;
-            }
-            
-        }
-        return -1;
+       int p=haystack.find(needle);
+       if(p>=0)
+       {
+           return p;
+       }
+       return -1;
     }
 };
