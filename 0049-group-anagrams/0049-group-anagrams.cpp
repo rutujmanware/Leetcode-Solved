fba87b3804656d6a63c15c113aeed56c2192a622
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-08-09 22:14:04 +0530

    Time: 33 ms (78.50%), Space: 19.8 MB (74.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,13 +2,25 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        map<vector<int>,vector<string>> m;
-        for(int i=0;i<strs.size();i++){
-            vector<int> c (26,0);
-            for(int j=0;j<strs[i].size();j++){
-                c[strs[i][j]-'a']++;
-            }
-            m[c].push_back(strs[i]); 
+        int n = strs.size();
+        // map<vector<int>,vector<string>> m;
+        // for(int i=0;i<strs.size();i++){
+        //     vector<int> c (26,0);
+        //     for(int j=0;j<strs[i].size();j++){
+        //         c[strs[i][j]-'a']++;
+        //     }
+        //     m[c].push_back(strs[i]); 
+        // }
+        // for(auto &v:m){
+        //     ans.push_back(v.second);
+        // }
+        // return ans;
+        
+        map<string,vector<string>> m;
+        for(int i=0;i<n;i++){
+            string dup = strs[i];
+            sort(dup.begin(),dup.end());
+            m[dup].push_back(strs[i]);
         }
         for(auto &v:m){
             ans.push_back(v.second);
