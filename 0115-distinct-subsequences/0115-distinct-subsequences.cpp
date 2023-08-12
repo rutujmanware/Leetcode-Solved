cabd5bd8efee0a4825594a07672e960052f59a8e
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-08-12 20:18:01 +0530

    Time: 42 ms (66.96%), Space: 18.5 MB (67.84%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int helper(int i,int j,string &s,string &t, vector<vector<int>> &dp){
-        if(j==t.size()){
-            return 1;
-        }
-        if(i==s.size()){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+//     int helper(int i,int j,string &s,string &t, vector<vector<int>> &dp){
+//         if(j==t.size()){
+//             return 1;
+//         }
+//         if(i==s.size()){
+//             return 0;
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
        
-        if(s[i]==t[j]){
-             return dp[i][j] = helper(i+1,j+1,s,t,dp) + helper(i+1,j,s,t,dp);
-        }
-       return dp[i][j] = helper(i+1,j,s,t,dp);
+//         if(s[i]==t[j]){
+//              return dp[i][j] = helper(i+1,j+1,s,t,dp) + helper(i+1,j,s,t,dp);
+//         }
+//        return dp[i][j] = helper(i+1,j,s,t,dp);
         
       
         
         
-    }
+//     }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return helper(0,0,s,t,dp);
+        vector<vector<unsigned int>> dp(s.size()+1,vector<unsigned int>(t.size()+1,0));
+        // return helper(0,0,s,t,dp);
+       
+        for(int i=0;i<=s.size();i++){
+            
+                dp[i][0] = 1;
+        }
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[s.size()][t.size()];
+        
     }
 };
