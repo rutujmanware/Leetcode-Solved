77a812f26414774f406e282c038b0c5b31d044b4
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-08-15 13:46:06 +0530

    Time: 7 ms (87.75%), Space: 5.9 MB (81.79%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int p = x;
+        long long int ans = 0;
+        if(x<0){
+            return false;
+        }
+        while(x){
+            int temp = x%10;
+            ans = ans*10+temp;
+            x /= 10;
+        }
+        if(p == ans){
+            return true;
+        }
+        return false;
+    }
+};
