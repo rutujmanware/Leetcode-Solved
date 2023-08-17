9bb0987ba421c7f52e468269726a772c9de8f7f1
Rituj Manware <79914270+rutujmanware@users.noreply.github.com>
2023-08-17 12:42:27 +0530

    Time: 2 ms (49.21%), Space: 5.9 MB (36.34%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,24 +1,38 @@
+// class Solution {
+// public:
+//     string convertToTitle(int columnNumber) {
+    
+//         string s = "";
+//         while(columnNumber > 26){
+//             int temp = columnNumber % 26;
+//             if(temp == 0){
+//                 s = 'Z' + s;
+//                 columnNumber /= 26;
+//                 columnNumber--;
+//             }
+//             else{
+                
+//                 s = char(temp + 'A' - 1) + s;
+//                 columnNumber /= 26;
+//             }
+//         }
+//         if(columnNumber){
+//             s = char(columnNumber + 'A' - 1) + s;
+//         }
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-    
-        string s = "";
-        while(columnNumber > 26){
-            int temp = columnNumber % 26;
-            if(temp == 0){
-                s = 'Z' + s;
-                columnNumber /= 26;
-                columnNumber--;
-            }
-            else{
-                
-                s = char(temp + 'A' - 1) + s;
-                columnNumber /= 26;
-            }
+        string ans ="";
+        while(columnNumber>0)
+        {
+            char ch = char(ceil((columnNumber-1)%26 +65));
+            ans = ch+ans;
+            columnNumber = (columnNumber-1)/26;
         }
-        if(columnNumber){
-            s = char(columnNumber + 'A' - 1) + s;
-        }
-        return s;
+        return ans;
     }
 };
